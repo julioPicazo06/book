a7f5ed74a294aa10f1b5c5375c9144685e9243d7
jpicazo <juliopicazo06@gmail.com>
2025-06-20 09:34:36 -0600

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,9 @@
 import { Menu } from "components/GeneralMenu"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 
 export const AppRoutes : React.FC = () => {
     return (
-        <Router basename="/book">
+        <Router>
             <Routes>
                 <Route path="/:param" element={<Menu/>}/>
                 <Route path="/" element={<Menu/>}/>
