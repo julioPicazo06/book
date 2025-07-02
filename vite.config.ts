5f12e59b27c2c7bfe3ee998c5d1e771714b34b94
jpicazo <juliopicazo06@gmail.com>
2025-07-01 20:25:23 -0600

    feat(config): update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,36 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/book/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'components': path.resolve(__dirname, 'src/components'),
-      'data': path.resolve(__dirname, 'src/data'),
-      'hooks': path.resolve(__dirname, 'src/hooks'),
-      'img': path.resolve(__dirname, 'src/img'),
-      'pdf': path.resolve(__dirname, 'src/pdf'),
-      'router': path.resolve(__dirname, 'src/router'),
-      'store': path.resolve(__dirname, 'src/store'),
-      'style': path.resolve(__dirname, 'src/style'),
-      'utils': path.resolve(__dirname, 'src/utils'),
-    }
-  },
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
-  build: {
-    outDir: 'docs',
-    assetsInlineLimit: 1048576, // 1MB
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/book/' : '/'
+  console.log(`Vite mode: ${mode}, base: ${base}`)
+  
+  return {
+    base,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        'components': path.resolve(__dirname, 'src/components'),
+        'data': path.resolve(__dirname, 'src/data'),
+        'hooks': path.resolve(__dirname, 'src/hooks'),
+        'img': path.resolve(__dirname, 'src/img'),
+        'pdf': path.resolve(__dirname, 'src/pdf'),
+        'router': path.resolve(__dirname, 'src/router'),
+        'store': path.resolve(__dirname, 'src/store'),
+        'style': path.resolve(__dirname, 'src/style'),
+        'utils': path.resolve(__dirname, 'src/utils'),
+      }
+    },
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
+    build: {
+      outDir: 'docs',
+      assetsInlineLimit: 1048576, // 1MB
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+        },
       },
     },
-  },
-  publicDir: 'public',
+    publicDir: 'public',
+  }
 }) 
